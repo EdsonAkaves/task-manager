e0a5c5a80db3c796fb5578d0ce42c06104838f18
Edson Alves <edson.akaves@gmail.com>
2025-08-08 16:47:08 -0300

    Add initial project files and README

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php 
+$host = 'localhost';
+$dbname = 'task_manager';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+
+?>
---
+++ src/models/Task.php
@@ -0,0 +1,56 @@
+<?php 
+require_once __DIR__ . '../../../config/database.php';
+
+
+class Task {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+    
+    public function getAllTasks() {
+        $stmt = $this->pdo->query("SELECT * FROM tasks ORDER BY created_at DESC");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function getTaskById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM tasks WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function createTask($title, $description) {
+        $stmt = $this->pdo->prepare("INSER INTO tasks (title, description, status, created_at)
+         VALUES (:title, :description, 'pending', NOW())");
+
+        return $stmt->execute([
+            'title' => $title,
+            'description' => $description
+        ]);
+
+     }
+
+    public function updateTask($id, $title, $description, $status) {
+        $stmt = $this->pdo->prepare("UPDATE tasks SET title = :title,
+         description = :description, status = :status WHERE id = :id");
+
+        return $stmt->execute([
+            'id' => $id,
+            'title' => $title,
+            'description' => $description,
+            'status' => $status
+        ]);    
+    }
+
+    public function deleteTask($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM tasks WHERE id = :id");
+        
+        return $stmt->execute(['id' => $id]);
+    }
+    
+}
+
+?>
